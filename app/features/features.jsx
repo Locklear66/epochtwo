632bf688b237c31ae568dda9128266b7f1c5b9c7
Lance <lance@Lances-MacBook-Pro.local>
2025-05-19 18:27:55 -0500

    pic update

--- app/features/features.jsx
+++ app/features/features.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import featurecss from './featurecss.module.css'
 import Image from 'next/image'
-import warrior from '/public/warrior.png'
+import warrior from '/public/warrior.webp'
 
 export default function features() {
   return (
--- app/main/main.jsx
+++ app/main/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import pagecss from './main.module.css'
 import Image from 'next/image'
-import background from "/public/background.png"
+import background from "/public/background.webp"
 
 export default function main() {
   return (
